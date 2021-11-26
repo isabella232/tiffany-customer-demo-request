ebec2f155a54ee88d63da231486e2498a11c2a10
Alexandre Viretti <alexou73@gmail.com>
2021-11-26 16:49:28 +0100

    fix search in shop list

--- src/Components/Header/ShopChoice.jsx
+++ src/Components/Header/ShopChoice.jsx
@@ -4,27 +4,28 @@ import { connectMenu } from 'react-instantsearch-dom';
 import { selectShop } from '../../actions/selectShop';
 
 const MenuSelect = ({ items, currentRefinement, refine }) => {
-    console.log('items', items);
     const dispatch = useDispatch();
     return (
-        <select
-            value={currentRefinement || ''}
-            onChange={event => {
-                refine(event.currentTarget.value);
-                dispatch(selectShop(event.currentTarget.value));
-            }}
-            className="shopSelection"
-        >
-            <option value="">Shop by location</option>
-            {items.map(item => (
-                <option
-                    key={item.label}
-                    value={item.isRefined ? currentRefinement : item.value}
-                >
-                    {item.label}
-                </option>
-            ))}
-        </select>
+        <div>
+            <input
+                list="brow"
+                onChange={e => {
+                    refine(e.currentTarget.value);
+                    dispatch(selectShop(e.currentTarget.value));
+                }}
+            />
+            <datalist id="brow">
+                <option value="Shop by location" />
+                {items.map(item => (
+                    <option
+                        key={item.label}
+                        value={item.isRefined ? currentRefinement : item.value}
+                    >
+                        {item.label}
+                    </option>
+                ))}
+            </datalist>
+        </div>
     );
 };
 
@@ -37,3 +38,27 @@ const ShopChoice = () => {
 };
 
 export default ShopChoice;
+
+{
+    /* <select
+value={currentRefinement || ''}
+onChange={event => {
+    refine(event.currentTarget.value);
+    dispatch(selectShop(event.currentTarget.value));
+}}
+className="shopSelection"
+>
+<input list="shops" name="shopsName" />
+<datalist id="shops">
+    <option value="">Shop by location</option>
+    {items.map(item => (
+        <option
+            key={item.label}
+            value={item.isRefined ? currentRefinement : item.value}
+        >
+            {item.label}
+        </option>
+    ))}
+</datalist>
+</select> */
+}
