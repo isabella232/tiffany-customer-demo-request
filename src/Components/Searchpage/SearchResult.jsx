c10bc9a81d2844eb69af081f1c824c3790246dda
Alexandre Viretti <alexou73@gmail.com>
2021-12-02 11:43:14 +0100

    fix display filters shop

--- src/Components/Searchpage/SearchResult.jsx
+++ src/Components/Searchpage/SearchResult.jsx
@@ -249,65 +249,83 @@ const CurrentRefinements = ({ items, refine }) => {
     const unique = uniqBy(items, 'currentRefinement');
     return (
         <ul className="refinement-content">
-            {unique.map(item => (
-                <li className="refinement-item" key={item.label}>
-                    {item.items ? (
-                        <React.Fragment>
-                            {/* <h3>{item.label}</h3> */}
-                            <ul className="refinement-results">
-                                {item.items.map(nested => (
-                                    <li key={nested.label}>
-                                        <a
-                                            className="refinement-filter"
-                                            href="#"
-                                            onClick={event => {
-                                                event.preventDefault();
-                                                refine(nested.value);
-                                            }}
-                                        >
-                                            {nested.label.includes('>')
-                                                ? nested.label.split('>')[1]
-                                                : nested.label}
-                                            <span
-                                                onClick={() => {
-                                                    refine(item.value);
-                                                }}
-                                            >
-                                                X
-                                            </span>
-                                        </a>
-                                    </li>
-                                ))}
-                            </ul>
-                        </React.Fragment>
-                    ) : (
-                        <>
-                            <a
-                                className="refinement-filter"
-                                href="#"
-                                onClick={event => {
-                                    event.preventDefault();
-                                    refine(item.value);
-                                }}
-                            >
-                                {item.attribute === 'PRICE_INT' ? (
-                                    <p>{item.label}</p>
-                                ) : (
-                                    <p>{item.currentRefinement}</p>
-                                )}
-                                <span
-                                    className="close-refinment"
-                                    onClick={() => {
+            {unique.map(item => {
+                return (
+                    <li className="refinement-item" key={item.label}>
+                        {item.items ? (
+                            <React.Fragment>
+                                {/* <h3>{item.label}</h3> */}
+                                <ul className="refinement-results">
+                                    {item.items.map(nested => {
+                                        return (
+                                            <li key={nested.label}>
+                                                <a
+                                                    className="refinement-filter"
+                                                    href="#"
+                                                    onClick={event => {
+                                                        event.preventDefault();
+                                                        refine(nested.value);
+                                                    }}
+                                                >
+                                                    {nested.label.includes('>')
+                                                        ? nested.label.split(
+                                                              '>'
+                                                          )[1]
+                                                        : nested.label}
+                                                    {nested.label.includes('//')
+                                                        ? nested.label.split(
+                                                              '//'
+                                                          )[1]
+                                                        : nested.label}
+                                                    <span
+                                                        onClick={() => {
+                                                            refine(item.value);
+                                                        }}
+                                                    >
+                                                        X
+                                                    </span>
+                                                </a>
+                                            </li>
+                                        );
+                                    })}
+                                </ul>
+                            </React.Fragment>
+                        ) : (
+                            <>
+                                <a
+                                    className="refinement-filter"
+                                    href="#"
+                                    onClick={event => {
+                                        event.preventDefault();
                                         refine(item.value);
                                     }}
                                 >
-                                    X
-                                </span>
-                            </a>
-                        </>
-                    )}
-                </li>
-            ))}
+                                    {item.attribute ===
+                                    'storeNameShopNumberV2' ? (
+                                        <p>
+                                            {
+                                                item.currentRefinement.split(
+                                                    '//'
+                                                )[1]
+                                            }
+                                        </p>
+                                    ) : (
+                                        <p>{item.currentRefinement}</p>
+                                    )}
+                                    <span
+                                        className="close-refinment"
+                                        onClick={() => {
+                                            refine(item.value);
+                                        }}
+                                    >
+                                        X
+                                    </span>
+                                </a>
+                            </>
+                        )}
+                    </li>
+                );
+            })}
         </ul>
     );
 };
